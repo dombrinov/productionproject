1426db8ec2fb955bcd44c12cf92c16d3d2d30f34
Kanur <ergo.proxy.ec@gmail.com>
2023-11-28 00:19:10 +0300

    Features-Sliced Design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,11 @@
 import { Suspense, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
-import { Counter } from "./components/Counter";
 import "./styles/index.scss";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "./providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -19,12 +17,10 @@ export const App = () => {
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={"/about"} element={<AboutPageAsync />} />
-          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPage />} />
+          <Route path={"/"} element={<MainPage />} />
         </Routes>
       </Suspense>
-
-      <Counter />
     </div>
   );
 };
