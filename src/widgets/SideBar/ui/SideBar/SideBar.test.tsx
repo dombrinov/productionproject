cdd73ad82284b8c81c70f714d5ab81a8ea8b5c7b
Kanur <ergo.proxy.ec@gmail.com>
2024-01-08 02:09:31 +0300

    fix test sidebar

--- src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
+++ src/widgets/SideBar/ui/SideBar/SideBar.test.tsx
@@ -1,15 +1,16 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { withTranslation } from "react-i18next";
+import { componentRender } from "shared/lib/tests/componentRender/componentRender";
 import { renderWithTranslation } from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
 import { SideBar } from "./SideBar";
 
 describe("SideBar", () => {
   test("test render button", () => {
-    renderWithTranslation(<SideBar />); //создал свою функию-хелпер, которая тянет конфиг i18nForTests для привлечения библиотеки для тестов
+    componentRender(<SideBar />); //создал свою функию-хелпер, которая тянет конфиг i18nForTests для привлечения библиотеки для тестов
     expect(screen.getByTestId("sideBar")).toBeInTheDocument(); //ожидаемый результат - появление сайдбара в документе
   });
   test("test toggle sideBar", () => {
-    renderWithTranslation(<SideBar />);
+    componentRender(<SideBar />);
     const toggleBtn = screen.getByTestId("sideBar-toggle");
     expect(screen.getByTestId("sideBar")).toBeInTheDocument();
     fireEvent.click(toggleBtn); //нажимаем на кнопку
